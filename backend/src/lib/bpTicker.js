5892d6f5e8c1368510bc063c10e3c240c5eb456e
leordev <leordev@gmail.com>
2018-07-04 08:56:50 -0400

    bp ticker retrieving bp jsons

--- backend/src/lib/bpTicker.js
+++ backend/src/lib/bpTicker.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 const EOS_NODE_API = process.env.EOS_NODE_API
 const TICK_INTERVAL = 60 * 1000
+const BP_JSON_TIMEOUT = 5000
 
 let bps = []
 
@@ -11,6 +12,8 @@ const tickLoop = async () => {
   console.log('BpTicker Init Loop')
 
   await initBps()
+
+  sortBps()
 
   await retrieveJsonData()
 
@@ -44,27 +47,68 @@ const updateBps = (bpRows) => {
   }
 
   bpRows.forEach(bp => {
-    let currentBp = bps.find(item => item.owner == bp.owner)
+    let currentBp = getBp(bp.owner)
 
-    if (!currentBp) {
+    const isValid = isValidBp(bp)
+
+    if (!currentBp && isValid) {
       console.log(`Adding new BP ${bp.owner}`)
       bps.push(bp)
-    } else {
+    } else if (currentBp && isValid) {
       console.log(`Updating and merging ${bp.owner}`)
       currentBp = {
         ...currentBp,
         ...bp
       }
+    } else if (currentBp && !isValid) {
+      console.log(`Removing invalid BP ${bp.owner}`)
+      bps.splice(bps.indexOf(currentBp), 1)
     }
   })
 }
 
-const retrieveJsonData = () => {
-  console.log('lazy developer need to do')
+const isValidBp = (bp) => {
+  return bp.url && Number(bp.total_votes) > 0 && bp.is_active == 1
+}
+
+const sortBps = () => {
+  bps.sort((a, b) => {
+    return Number(b.total_votes) - Number(a.total_votes)
+  })
+}
+
+const retrieveJsonData = async () => {
+  const jsonsRequests = bps.map(bp => {
+    const url = bp.url +
+      (bp.url.substr(-1) === '/' ? 'bp.json' : '/bp.json')
+    return axios.get(url, {timeout: BP_JSON_TIMEOUT})
+      .then(res => ({owner: bp.owner, data: res.data}))
+      .catch(e => ({owner: bp.owner, error: e}))
+  })
+
+  console.log(`Getting BP JSONs for ${jsonsRequests.length} bps`)
+  const jsonsRes = await Promise.all(jsonsRequests)
+    .catch(e => console.log('Fail to get BP JSONs Files', e))
+
+  if (jsonsRes) {
+    jsonsRes.forEach(json => {
+      if (json.error) {
+        console.log(`${json.owner} - Failed JSON Response`)
+      } else {
+        const bp = getBp(json.owner)
+        if (bp)
+          bp.json = json.data
+      }
+    })
+  }
 }
 
 const getBps = () => {
   return bps
+}
+
+const getBp = owner => {
+  return bps.find(bp => bp.owner == owner)
 }
 
 // initialize loop
